3b16e9b185b06127ed4813ced539e02113046b36
Administrator <Administrator@DESKTOP-I84IGVI>
2022-01-22 15:54:50 +0000

    Added BenchmarkDotNet

---
+++ NuGetRestore.Benchmark/Program.cs
@@ -0,0 +1,40 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System.Security.Cryptography;
+
+namespace NuGetRestore.ConsoleApp
+{
+    /// <summary>
+    /// This console app must be run in Release mode for the benchmark to run. It will not work in Debug!
+    /// </summary>
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<Md5VsSha256>();
+            Console.WriteLine(summary);
+        }
+    }
+
+    public class Md5VsSha256
+    {
+        private const int _arraySize = 10000;
+        private readonly byte[] _data;
+
+        private readonly SHA256 _sha256 = SHA256.Create();
+        private readonly MD5 _md5 = MD5.Create();
+
+        public Md5VsSha256()
+        {
+            _data = new byte[_arraySize];
+            new Random(42).NextBytes(_data);
+        }
+
+        [Benchmark]
+        public byte[] Sha256() => _sha256.ComputeHash(_data);
+
+        [Benchmark]
+        public byte[] Md5() => _md5.ComputeHash(_data);
+    }
+}
+
